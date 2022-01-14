08908270dd44053a121b3a0e2efaec38dd17a7c6
E-K8 <ekatplat@gmail.com>
2022-01-14 13:48:54 +0000

    changed alt string in img

--- src/Components/PlanetDisplay/index.js
+++ src/Components/PlanetDisplay/index.js
@@ -4,7 +4,7 @@ import earth from "../../public/earth.jpg";
 export function PlanetDisplay() {
   return (
     <article>
-      <img src={earth} alt="photo of Earth" />
+      <img src={earth} alt="Earth" />
       <h1>Earth</h1>
       <p>Facts and stuff</p>
       <p>Moons and info</p>
